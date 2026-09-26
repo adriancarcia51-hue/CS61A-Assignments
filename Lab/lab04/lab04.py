903478536e002916372e81f71e85588850a005b3
adriancarcia51-hue <adriancarcia51@gmail.com>
2026-09-26 15:47:44 +0800

    完成lab04

--- Lab/lab04/lab04.py
+++ Lab/lab04/lab04.py
@@ -7,7 +7,7 @@ def divide(quotients, divisors):
     >>> divide(range(1, 5), range(20, 25))
     {1: [20, 21, 22, 23, 24], 2: [20, 22, 24], 3: [21, 24], 4: [20, 24]}
     """
-    return {____: ____ for ____ in ____}
+    return {i:[k for k in divisors if k%i==0] for i in quotients}
 
 
 def buy(fruits_to_buy, prices, total_amount):
@@ -29,10 +29,10 @@ def buy(fruits_to_buy, prices, total_amount):
             print(cart)
         elif fruits and amount > 0:
             fruit = fruits[0]
-            price = ____
-            for k in ____:
+            price = prices[fruit]
+            for k in range(1,amount//price+1):
                 # Hint: The display function will help you add fruit to the cart.
-                add(____, ____, ____)
+                add(fruits[1:], amount-k*price, cart+display(fruit,k))
     add(fruits_to_buy, total_amount, '')
 
 
@@ -67,6 +67,9 @@ def distance(city_a, city_b):
     5.0
     """
     "*** YOUR CODE HERE ***"
+    xa,ya=get_lat(city_a),get_lon(city_a)
+    xb,yb=get_lat(city_b),get_lon(city_b)
+    return sqrt((xa-xb)**2+(ya-yb)**2)
 
 def closer_city(lat, lon, city_a, city_b):
     """
@@ -84,6 +87,8 @@ def closer_city(lat, lon, city_a, city_b):
     'Bucharest'
     """
     "*** YOUR CODE HERE ***"
+    c=make_city("name",lat,lon)
+    return get_name(city_a if distance(c,city_a)<=distance(c,city_b) else city_b)
 
 def check_city_abstraction():
     """
